5cd294b7e58245383b016f5de1830221bac09d77
Chris Hallberg <crhallberg@gmail.com>
2018-01-27 07:52:15 +0000

    Shifters locked and loaded.

--- firebase.js
+++ firebase.js
@@ -2,9 +2,9 @@ let CONFIG = {};
 
 const xhr = new XMLHttpRequest();
 xhr.addEventListener('load', function loadConfig() {
-  CONFIG = JSON.parse(this.responseText);
-  firebase.initializeApp(CONFIG.firebase);
-  document.dispatchEvent(new Event('config-loaded'));
+	CONFIG = JSON.parse(this.responseText);
+	firebase.initializeApp(CONFIG.firebase);
+	document.dispatchEvent(new Event('config-loaded'));
 });
 xhr.open('GET', './config.json');
 xhr.send();
